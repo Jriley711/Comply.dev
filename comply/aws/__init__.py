c7740fb3f7a703f914dfd0b34ea4be52a917edd2
Jriley711 <Jonathan@rileyorchards.com>
2026-06-01 15:11:37 -0700

    Update __init__.py

--- comply/aws/__init__.py
+++ comply/aws/__init__.py
@@ -1,1 +1,12 @@
 # This file marks the directory as a Python package.
+from .security_groups import SecurityGroupScanner
+from .encryption import EncryptionScanner
+from .backups import BackupScanner
+from .iam import IAMScanner
+
+__all__ = [
+    "SecurityGroupScanner",
+    "EncryptionScanner",
+    "BackupScanner",
+    "IAMScanner",
+]
